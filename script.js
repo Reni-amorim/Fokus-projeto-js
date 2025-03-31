56ec2748b22d31b08e7f25ada0e11074bb4dbd49
Reni-amorim <reni.425@gmail.com>
2025-03-31 12:48:57 -0300

    Add text and audio features

--- script.js
+++ script.js
@@ -1,24 +1,72 @@
+const timerFoco = 1500;
+const timerCurto = 300;
+const timerLongo = 900;
+
 const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
 const displayTimer = document.querySelector('#timer')
-const banner = document.querySelector('.app__img')
+const banner = document.querySelector('.app__image')
 const apptitle = document.querySelector('.app__title')
 const startStopBt = document.querySelector('.app__card-primary-button')
 const StartStopBtFoco = document.querySelector('.app__card-button--foco')
 const StartStopBtCurto = document.querySelector('.app__card-button--curto')
 const StartStopBtLongo = document.querySelector('.app__card-button--longo')
+const btn = document.querySelectorAll('.app__card-button')
+const songFoco = document.querySelector('#alternar-musica')
+const song = new Audio('/sons/luna-rise-part-one.mp3')
+song.loop = true
 
+
+
+songFoco.addEventListener('change', () => {
+    if(song.paused) {
+        song.play()
+    } else {
+        song.pause()
+    }
+})
 
 focoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    alterarContexto('foco')
+    focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
+
+function alterarContexto(contexto) {
+    btn.forEach(function (contexto){
+        contexto.classList.remove('active')
+    })
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case 'foco':
+            apptitle.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case 'descanso-curto':
+            apptitle.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+
+        case 'descanso-longo':
+            apptitle.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+            break;           
+    }
+}
